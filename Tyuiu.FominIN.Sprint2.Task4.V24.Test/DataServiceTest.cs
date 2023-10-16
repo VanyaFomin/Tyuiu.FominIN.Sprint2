6b9ed2cf3cf8deb1fe68782d8f0d9ff9ac7b9401
Tyuiu.FominVN@outlook.com <hialdez111@gmail.com>
2023-10-16 21:38:57 +0500

    Task4

---
+++ Tyuiu.FominIN.Sprint2.Task4.V24.Test/DataServiceTest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.FominIN.Sprint2.Task4.V24.Lib;
+
+namespace Tyuiu.FominIN.Sprint2.Task4.V24.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 5;
+            double y = 10;
+            double res = ds.Calculate(x, y);
+            double wait = 112.445;
+            Assert.AreEqual(wait, res);
+
+        }
+
+        [TestMethod]
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 10;
+            double y = 15;
+            double res = ds.Calculate(x, y);
+            double wait = 237.794;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.FominIN.Sprint2.Task4.V24/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.FominIN.Sprint2.Task4.V24.Lib;
+
+namespace Tyuiu.FominIN.Sprint2.Task4.V24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Фомин И. Н. | ПКТб-23-1";
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* Спринт #2                                                                    *");
+            Console.WriteLine("* Тема: Алгоритмы разветвляющейся структуры                                    *");
+            Console.WriteLine("* Задание #4                                                                   *");
+            Console.WriteLine("* Вариант #24                                                                  *");
+            Console.WriteLine("* Выполнил: Фомин Иван Николаевич | ПКТб-23-1                                  *");
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                     *");
+            Console.WriteLine("* Написать программу, которая вычисляет требуемое значение с использованием    *");
+            Console.WriteLine("* тернарнорного оператора, где пользователь вводит значение x, y с клавиатуры  *");
+            Console.WriteLine("* Округлить полученное значение до трех знаков после запятой                   *");
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                             *");
+            Console.WriteLine("********************************************************************************");
+
+            Console.WriteLine("Введите значение переменной X: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение переменной Y: ");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            double res = ds.Calculate(x, y);
+
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                   *");
+            Console.WriteLine("********************************************************************************");
+
+            Console.WriteLine("Значение функции = " + Math.Round(res, 3));
+
+            Console.ReadKey();
+        }
+    }
+    
+}
